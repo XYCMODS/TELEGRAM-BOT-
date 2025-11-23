5dc54ee06de94d40ba68132bf533237099c29378
XYCMODS <primebala644@gmail.com>
2025-11-24 04:38:45 +0530

    Delete products.php

--- products.php
+++
@@ -1,5 +0,0 @@
-<?php include 'header.php'; ?>
-<h2>Products</h2>
-<div class='product'><img src='https://via.placeholder.com/150'><h3>Product 1</h3><p>₹499</p></div>
-<div class='product'><img src='https://via.placeholder.com/150'><h3>Product 2</h3><p>₹799</p></div>
-<?php include 'footer.php'; ?>
